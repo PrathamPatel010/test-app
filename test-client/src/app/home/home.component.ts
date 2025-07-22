8080496dbae11db578c593c3f2f6adf8c398f5e1
Pratham Patel <ppratham812@gmail.com>
2025-07-22 23:35:08 +0530

    added changes

--- test-client/src/app/home/home.component.ts
+++ test-client/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   title = 'Angular';
+  apiUrl = environment.API_URL;
 }
