6af2d2c79bb75b1af9238580aef286425e096fad
Pratham Patel <ppratham812@gmail.com>
2025-07-22 22:41:03 +0530

    init

---
+++ api/AppSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace api
+{
+    public class AppSettings
+    {
+        public string JwtSecret { get; set; } = string.Empty;
+        public string JwtExpiryMinutes { get; set; } = string.Empty;
+    }
+}
---
+++ api/Controllers/SecretController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("api/secrets")]
+    public class SecretController : ControllerBase
+    {
+        private readonly AppSettings _appSettings;
+
+        public SecretController(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+        }
+
+        [HttpGet]
+        public IActionResult GetSecret()
+        {
+            return Ok(new { Secret = _appSettings.JwtSecret, Expiry = int.Parse(_appSettings.JwtExpiryMinutes) });
+        }
+    }
+}
