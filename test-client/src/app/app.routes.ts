d4363d6a3e9a84c287511b14b3ec65c6c55b8bb9
Pratham Patel <ppratham812@gmail.com>
2025-07-22 23:02:56 +0530

    updated ng client

--- test-client/src/app/app.routes.ts
+++ test-client/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ChildComponent } from './child/child.component';
+import { AppComponent } from './app.component';
+import { ParentComponent } from './parent/parent.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'parent', component: ParentComponent },
+  { path: 'child', component: ChildComponent },
+];
